d9056b9a66d4f0da4460074f5edc0c6fb83747e9
capinzor <capinzonr@unal.edu.co>
2020-04-09 16:39:32 -0500

    Finish add  words and images

--- 18-angular/ahorcado/src/app/app.component.ts
+++ 18-angular/ahorcado/src/app/app.component.ts
@@ -8,11 +8,35 @@ export class AppComponent {
   palabra = 'AGUACATE';
   palabraOculta = '';
 
+  intentos = 0;
+
   letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
-  'K', 'L', 'M', 'N', 'Ã‘', 'O', 'P', 'Q', 'R', 'S',
+  'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S',
   'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
   constructor(){
     this.palabraOculta = '_ '.repeat(this.palabra.length);
   }
+
+  comprobar(letra){
+    this.existeLetra(letra);
+    const palabraOcultaArr = this.palabraOculta.split(' ');
+    for(let i=0;i<this.palabra.length; i++){
+      if(this.palabra[i]===letra){
+        palabraOcultaArr[i] = letra;
+      }
+    }
+    this.palabraOculta = palabraOcultaArr.join(' ');
+    console.log(palabraOcultaArr);
+  }
+
+  existeLetra(letra){
+    if(this.palabra.indexOf(letra)>=0){
+      console.log('letra existe');
+    }else{
+      console.log('letra no existe');
+      this.intentos++;
+    }
+  }
+
 }
